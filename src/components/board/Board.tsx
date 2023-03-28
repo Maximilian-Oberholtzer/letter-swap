79d96b2f9fd4ab5296c16fe2eb37432254bfe679
Maximilian Oberholtzer <max.oberholtzer@yahoo.com>
2023-03-28 15:28:45 -0400

    Slightly adjusted weights for letters

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -11,8 +11,8 @@ const getRandomLetter = (): string => {
   const alphabet: Letter[] = "abcdefghijklmnopqrstuvwxyz".split("");
   const weights: number[] = [
     8.167, 1.492, 2.782, 4.253, 12.702, 2.228, 2.015, 6.094, 6.966, 0.153,
-    0.772, 4.025, 2.406, 6.749, 7.507, 1.929, 0.095, 5.987, 6.327, 9.056, 2.758,
-    0.978, 2.36, 0.15, 1.974, 0.074,
+    0.772, 4.025, 2.406, 6.749, 7.507, 1.929, 0.095, 5.987, 10.702, 9.056,
+    2.758, 0.978, 2.36, 0.15, 1.974, 0.074,
   ];
 
   function weightedRandomIndex(weights: number[]): number {
