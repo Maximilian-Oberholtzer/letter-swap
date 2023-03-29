e90a53012dfce61352e20dd4eb617b58ebbd2411
Maximilian Oberholtzer <max.oberholtzer@yahoo.com>
2023-03-29 16:08:17 -0400

    Game session now saves current data with local storage

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -5,10 +5,16 @@ function Main() {
   return (
     <div className="title-container">
       <div className="title">
-        <div className="title-tile">L</div>etter
+        <div className="title-tile animate" style={{ animationDelay: "0.5s" }}>
+          L
+        </div>
+        etter
       </div>
       <div className="title" style={{ marginTop: "1rem" }}>
-        <div className="title-tile">S</div>wap
+        <div className="title-tile animate" style={{ animationDelay: "0.8s" }}>
+          S
+        </div>
+        wap
       </div>
     </div>
   );
