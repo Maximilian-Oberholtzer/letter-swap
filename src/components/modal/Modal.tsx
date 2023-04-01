761c6d7b0ce69fb81b440c76083a4d00c4a2bbaa
Max Oberholtzer <maxoberholtzer@Maxs-MacBook-Pro.local>
2023-04-01 13:31:08 -0400

    Added logic for once a day play, statistics modal with share button

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -6,14 +6,29 @@ interface ModalProps {
   score: number;
   onClose: () => void;
   reset: () => void;
+  weeklyScores: (number | null)[];
 }
+
+const handleShare = async (score: number) => {
+  if (navigator.share) {
+    try {
+      await navigator.share({
+        title: "LetterSwap",
+        text: `I found ${score} word(s) today in LetterSwap.`,
+        url: window.location.href,
+      });
+    } catch (error) {
+      console.error("Error sharing:", error);
+    }
+  } else {
+    console.warn("Web Share API not supported on this device.");
+  }
+};
 
 const howToPlay = (
   <>
     <h1 className="modal-title">How To Play</h1>
-    <p className="modal-subtitle">
-      Create as many 5-letter words as possible
-    </p>
+    <p className="modal-subtitle">Create as many 5-letter words as possible</p>
     <ul style={{ paddingInlineStart: "20px" }}>
       <li className="how-to-play-instructions">
         Tap on a tile to replace it with the next letter.
@@ -36,9 +51,71 @@ const howToPlay = (
   </>
 );
 
-const gameOver = (score: number) => <p>You found {score} word(s)</p>;
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const currentDay = new Date().getDay();
 
-const Modal: React.FC<ModalProps> = ({ type, score, onClose, reset }) => {
+const statistics = (score: number, weeklyScores: (number | null)[]) => (
+  <>
+    <h1 className="modal-title">Statistics</h1>
+    <p className="modal-subtitle">
+      {score !== 1 ? (
+        <>Your score today: {score} words.</>
+      ) : (
+        <>Your score today: {score} word.</>
+      )}
+    </p>
+    <div className="weekly-score-container">
+      {days.map((day, i) => {
+        return (
+          <p key={i} className="weekly-score-text">
+            {i === currentDay ? (
+              <b>
+                {day}: {weeklyScores[i] !== null ? weeklyScores[i] : "---"}
+              </b>
+            ) : (
+              <>
+                {day}: {weeklyScores[i] !== null ? weeklyScores[i] : "---"}
+              </>
+            )}
+          </p>
+        );
+      })}
+    </div>
+    <div className="modal-bottom-container">
+      <button
+        onClick={() => {
+          handleShare(score);
+        }}
+        className="share-button"
+      >
+        <div className="share-button-container">
+          <b>Share</b>{" "}
+          <svg
+            width="2rem"
+            height="2rem"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M9.61109 12.4L10.8183 18.5355C11.0462 19.6939 12.6026 19.9244 13.1565 18.8818L19.0211 7.84263C19.248 7.41555 19.2006 6.94354 18.9737 6.58417M9.61109 12.4L5.22642 8.15534C4.41653 7.37131 4.97155 6 6.09877 6H17.9135C18.3758 6 18.7568 6.24061 18.9737 6.58417M9.61109 12.4L18.9737 6.58417M19.0555 6.53333L18.9737 6.58417"
+              stroke="#000000"
+              strokeWidth="1.5"
+            />
+          </svg>
+        </div>
+      </button>
+    </div>
+  </>
+);
+
+const Modal: React.FC<ModalProps> = ({
+  type,
+  score,
+  onClose,
+  reset,
+  weeklyScores,
+}) => {
   const handleOverlayClick = (event: React.MouseEvent<HTMLDivElement>) => {
     closeModal();
   };
@@ -48,11 +125,11 @@ const Modal: React.FC<ModalProps> = ({ type, score, onClose, reset }) => {
     modal?.classList.add("closed");
     setTimeout(() => {
       onClose();
+      if (type === "game-over") {
+        reset();
+      }
     }, 300);
-    //temporary - reset when user completes game
-    if (type === "game-over") {
-      reset();
-    }
+    // //temporary - reset when user completes game
   };
 
   return (
@@ -66,8 +143,8 @@ const Modal: React.FC<ModalProps> = ({ type, score, onClose, reset }) => {
           }}
         >
           <svg
-            width="1.5rem"
-            height="1.5rem"
+            width="1.25rem"
+            height="1.25rem"
             viewBox="0 0 24 24"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
@@ -85,7 +162,7 @@ const Modal: React.FC<ModalProps> = ({ type, score, onClose, reset }) => {
           </svg>
           {/* <img alt="" src={closeSvg} className="close-button-img" /> */}
         </button>
-        {type === "game-over" && gameOver(score)}
+        {type === "game-over" && statistics(score, weeklyScores)}
         {type === "how-to-play" && howToPlay}
       </div>
     </div>
